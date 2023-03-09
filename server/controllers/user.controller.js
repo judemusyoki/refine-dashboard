a145dcd090fcf671cb7a533fd64f1d1d76b68282
judemusyoki <judemusyoki@gmail.com>
2023-03-09 11:53:31 +0100

    create routes & controllers

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import Property from '../mongodb/models/property.js'
+import User from '../mongodb/models/user.js'
+
+const getAllUsers = async (req, res) => {}
+const createUser = async (req, res) => {}
+const getUserInfoByID = async (req, res) => {}
+
+export { getAllUsers, createUser, getUserInfoByID }
