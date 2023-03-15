7d6dfcc29753f65134dccb1f73cf32ebc1dd1f2d
judemusyoki <judemusyoki@gmail.com>
2023-03-15 01:50:02 +0100

    agent profile

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -1,7 +1,31 @@
+import { useOne } from '@pankod/refine-core'
+import { useParams } from '@pankod/refine-react-router-v6'
+import { Profile } from 'components'
+
 import React from 'react'
 
 const AgentProfile = () => {
-  return <div>agent-profile</div>
+  const { id } = useParams()
+
+  const { data, isLoading, isError } = useOne({
+    resource: 'users',
+    id: id as string,
+  })
+
+  const myProfile = data?.data ?? []
+
+  if (isLoading) return <div>loading...</div>
+  if (isError) return <div>error...</div>
+
+  return (
+    <Profile
+      type="Agent"
+      name={myProfile.name}
+      email={myProfile.email}
+      avatar={myProfile.avatar}
+      properties={myProfile.allProperties}
+    />
+  )
 }
 
 export default AgentProfile
